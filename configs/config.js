d03454d61cb7cea0d3636b97174109dbad1d8018
Kevin Valentine <kvalentine@benchmarkeducation.com>
2018-09-04 13:44:55 -0400

    Actually sending up changes now

--- configs/config.js
+++ configs/config.js
@@ -5,8 +5,13 @@ module.exports = {
       mapFieldTo: 'Description',
     },
     {
-      fieldInXML: 'Name._text',
-      mapFieldTo: 'Summary',
+      fieldInXML: 'Description._text',
+      mapFieldTo: 'Description',
+    },
+    {
+      staticValue: 'Story',
+      mapFieldTo: 'IssueType',
+      addColumn: true,
     },
     {
       fieldInXML: 'Attachments._itemRefArray.Attachment',
@@ -14,6 +19,22 @@ module.exports = {
       isArray: {
         fieldInXML: '_attributes.ref',
         addColumn: true,
+      }
+    },
+    {
+      fieldInXML: 'Tasks._itemRefArray.Task',
+      isArray{
+        addRow: true,
+        fieldsToGrab: [
+          {
+            fieldInXML: '_attributes.refObjectName'
+            mapFieldTo: 'Summary'
+          },
+          {
+            mapFieldTo: 'IssueType',
+            staticValue: 'SubTask',
+          }
+        ],
       }
     }
   ],
